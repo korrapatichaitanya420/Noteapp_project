188d0f7554e89608b22c1e570f4512c0e4b1d446
korrapatichaitanya420 <115769796+korrapatichaitanya420@users.noreply.github.com>
2022-11-18 03:07:06 -0500

    Add files via upload

--- mainproject.js
+++ mainproject.js
@@ -1,3 +1,12 @@
+// getUsers button 
+ document.getElementById("btn-users").addEventListener('click', getUsers);
+ function getUsers() {
+   fetch("http://localhost:3000/users/")
+  .then((res)=> res.json())
+  .then((data) => console.log(data))
+  .catch((err)=> console.log(err))
+ }
+
 const login = document.getElementById("loginpage");
 const register = document.getElementById("rForm");
 const note = document.getElementById("nForm")
